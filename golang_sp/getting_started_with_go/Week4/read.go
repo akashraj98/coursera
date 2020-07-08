3f4ccece73a9e3113250dda20063f3c7119fe721
Akash Raj <akashraj7713@gmail.com>
2020-07-09 01:17:42 +0530

    some changes

--- golang_sp/getting_started_with_go/Week4/read.go
+++ golang_sp/getting_started_with_go/Week4/read.go
@@ -19,14 +19,16 @@ func main(){
     dat,err := ioutil.ReadFile("./"+filename)
     if err != nil{ panic(err)}
 	data := strings.Split(string(dat),"\n")
-	data = data[:len(data)-1]
+	//data = data[:len(data)-1]
 	for _,v := range data{
+		if v ==""{break}
 		var p person
 		p.fname = strings.Split(v," ")[0]
 		p.lname = strings.Split(v," ")[1]
 		sli= append(sli,p)
 		}
-	for _,v:= range sli{
-		fmt.Println(v)
+	for i,v:= range sli{
+		fmt.Println("Person",i+1)
+		fmt.Println("Firstname:",v.fname," Lastname:",v.lname)
 		}
     }
